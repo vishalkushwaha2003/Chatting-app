1fe1e4f032870d94cd191b30ed0037b79f2f38c8
vishalkushwaha2003 <vishal2003kushwaha@gmail.com>
2024-04-25 22:20:27 +0530

    create getmessage route

--- backend/controllers/message.controllers.js
+++ backend/controllers/message.controllers.js
@@ -42,3 +42,24 @@ export const sendMessage=async(req,res)=>{
         res.status(500).json({error: "internal server error"})
      }
 }
+
+export const getMessages=async(req,res)=>{
+    try {
+        const {id:userToChatId}=req.params;
+        const senderId=req.user._id;
+
+        const conversation=await Conversation.findOne({
+            participants:{$all:[userToChatId,senderId]},
+        }).populate("messages");
+
+        if(!conversation) return res.status(200).json([]);
+
+        const messages=conversation.messages;
+
+        res.status(201).json(messages);
+        
+    } catch (error) {
+        console.log("error in getMessage controller ,",error.message)
+       return res.status(500).json({error:"internal server error"})
+    }
+}
--- backend/routes/message.routes.js
+++ backend/routes/message.routes.js
@@ -1,10 +1,10 @@
 import express from "express"
-import { sendMessage } from "../controllers/message.controllers.js";
+import { sendMessage,getMessages } from "../controllers/message.controllers.js";
 import protectRoute from "../middleware/protectRoute.js";
 
 
 const router=express.Router();
-
+router.get("/:id",protectRoute,getMessages)
 router.post("/send/:id",protectRoute,sendMessage)
 
 export default router;
