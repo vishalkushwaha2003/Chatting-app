967434b03b1400989c33b0796e1562f3648bd81a
vishalkushwaha2003 <vishal2003kushwaha@gmail.com>
2024-04-29 14:51:26 +0530

    fetch the sidebar users data

--- frontend/src/components/sidebar/Conversation.jsx
+++ frontend/src/components/sidebar/Conversation.jsx
@@ -1,26 +1,26 @@
 import React from 'react'
 
 
-function Conversation() {
+function Conversation({conversation,lastIndex}) {
   return (
     <>
     <div className='flex gap-2 items-center hover:bg-sky-500 rounded p-2 py-1 cursor-pointer'>
 
  <div className='avatar online'>
     <div className='w-12 rounded-full'>
-        <img src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" alt="user avatar" />
+        <img src={conversation.profilePic} alt="user avatar" />
     </div>
 
  </div>
 
    <div className='flex flex-col flex-1'>
  <div className='flex gap-3 justify-between'>
-  <p className='font-bold text-grey-200'>Vishal</p>
+  <p className='font-bold text-grey-200'>{conversation.fullName}</p>
   <span className='text-xl '> ❤️</span>
  </div>
    </div>
     </div>
-    <div className='divider my-0 py-0 h-1'/>
+    {!lastIndex&&<div className='divider my-0 py-0 h-1'/>}
     </>
   )
 }
---
+++ frontend/src/hooks/useGetConversations.js
@@ -0,0 +1,42 @@
+import React, { useState,useEffect } from 'react'
+import {toast} from 'react-hot-toast'
+
+function useGetConversations() {
+
+   const [loading,setLoading]=useState(false);
+   const[conversations,setConversations]=useState([]);
+
+   useEffect(()=>{
+
+     const getConversations=async()=>{
+        setLoading(true);
+        try {
+             
+         const res=await fetch("/api/users");
+         const data= await res.json();
+         if(data.error){
+            throw new Error(data.error);
+         }
+
+
+          setConversations(data);
+           
+
+        } catch (error) {
+            toast.error(error.message);
+        }
+        finally{
+            setLoading(false);
+        }
+
+     }
+
+        getConversations();
+   },[])
+
+
+   return{loading,conversations}
+ 
+}
+
+export default useGetConversations;
