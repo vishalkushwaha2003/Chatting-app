61889a6e2c6a0600426edb5665d634e57a1496c5
vishalkushwaha2003 <vishal2003kushwaha@gmail.com>
2024-04-30 07:51:34 +0530

    change get information for login

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -19,6 +19,7 @@ export const login=async(req,res)=>{
 
       generateTokenAndSetCookie(user._id,res);
       res.status(201).json({
+        _id:user._id,
         fullName:user.fullName,
         userName:user.userName,
         profilePic:user.profilePic
