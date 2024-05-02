6dcde931f252edb90d194849df0c7cc3a01f7fa1
vishalkushwaha2003 <vishal2003kushwaha@gmail.com>
2024-05-02 16:44:52 +0530

    change socket url

--- backend/soket/socket.js
+++ backend/soket/socket.js
@@ -6,7 +6,7 @@ const app =express();
 const server=http.createServer(app);
 const io=new Server(server,{
     cors:{
-        origin:["http://localhost:3000"],
+        origin:["https://chatting-app-ouxe.onrender.com"],
         methods:["GET","POST"],
     },
 })
